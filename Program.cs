f3da4e347e7305b13f5c9109f13b44246ff20669
Jeas0001 <jeas0001@edu.mercantec.dk>
2024-05-02 13:39:18 +0200

    Only adds unique first and last name

--- Program.cs
+++ Program.cs
@@ -8,18 +8,26 @@ using var sdb = new SQLConn.DBContext();
 
 var mEntities = mdb.entities.Include(e => e.City).ToList();
 var sEntities = sdb.entities.Include(e => e.City).ToList();
-
+int i = 0;
 foreach (var entity in mEntities)
 {
-    if (!sEntities.Contains(entity))
+    bool flag = false;
+    foreach (var entity2 in sEntities)
     {
+        if (entity.FirstName != entity2.FirstName && entity.LastName != entity2.LastName && entity.ID != entity2.ID) { flag = true; }
+    }
+    if (flag)
+    {
+        i++;
         sdb.Add(new Person() { City = new City() { CityName = entity.City.CityName, PostalCode = entity.City.PostalCode }, FirstName = entity.FirstName, Jobtitle = entity.Jobtitle, LastName = entity.LastName });
     }
 }
 sdb.SaveChanges();
+Console.WriteLine($"{i} rows added");
+Console.ReadKey();
 
 
-//var person1 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person1", Jobtitle = "Student", LastName = "Askaa" };
+//var person1 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "test", Jobtitle = "Student", LastName = "test" };
 //var person2 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person2", Jobtitle = "Student", LastName = "Askaa" };
 //var person3 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person3", Jobtitle = "Student", LastName = "Askaa" };
 //var person4 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person4", Jobtitle = "Student", LastName = "Askaa" };
