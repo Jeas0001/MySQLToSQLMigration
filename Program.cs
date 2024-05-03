f10d1d7b40fc827b2fcf2173de62b73e5053c674
Jeas0001 <jeas0001@edu.mercantec.dk>
2024-05-03 09:58:05 +0200

    Fixes the problem

--- Program.cs
+++ Program.cs
@@ -11,18 +11,18 @@ var sEntities = sdb.entities.Include(e => e.City).ToList();
 int i = 0;
 foreach (var entity in mEntities)
 {
+    bool flag = false;
     foreach (var entity2 in sEntities)
     {
-        bool flag = false;
         if (entity.FirstName != entity2.FirstName && entity.LastName != entity2.LastName) { flag = true; }
         if (entity.FirstName == entity2.FirstName && entity.LastName != entity2.LastName) { flag = true; }
         if (entity.FirstName != entity2.FirstName && entity.LastName == entity2.LastName) { flag = true; }
+    }
 
-        if (flag)
-        {
-            i++;
-            sdb.Add(new Person() { City = new City() { CityName = entity.City.CityName, PostalCode = entity.City.PostalCode }, FirstName = entity.FirstName, Jobtitle = entity.Jobtitle, LastName = entity.LastName });
-        }
+    if (flag)
+    {
+        i++;
+        sdb.Add(new Person() { City = new City() { CityName = entity.City.CityName, PostalCode = entity.City.PostalCode }, FirstName = entity.FirstName, Jobtitle = entity.Jobtitle, LastName = entity.LastName });
     }
 }
 sdb.SaveChanges();
@@ -30,7 +30,7 @@ Console.WriteLine($"{i} rows added");
 Console.ReadKey();
 
 
-//var person1 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "test", Jobtitle = "Student", LastName = "test" };
+//var person1 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "test1", Jobtitle = "Student", LastName = "test" };
 //var person2 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person2", Jobtitle = "Student", LastName = "Askaa" };
 //var person3 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person3", Jobtitle = "Student", LastName = "Askaa" };
 //var person4 = new Person() { City = new City() { CityName = "Silkeborg", PostalCode = 8600 }, FirstName = "person4", Jobtitle = "Student", LastName = "Askaa" };
--- obj/Debug/net6.0/MySQLToSQLMigration.AssemblyInfo.cs
+++ obj/Debug/net6.0/MySQLToSQLMigration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySQLToSQLMigration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6a569d4fd0545941d0d99bdbeed2a81dd3b08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e29e50d16abcc292bbe07cee230e724542c333")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySQLToSQLMigration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySQLToSQLMigration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
